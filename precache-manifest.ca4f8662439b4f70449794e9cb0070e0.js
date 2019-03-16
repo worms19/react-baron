bb8931386e8c1bf911de53aac523219b90def069
worms19 <anselmidamien@gmail.com>
2019-03-16 17:34:02 +0100

    Updates

---
+++ precache-manifest.ca4f8662439b4f70449794e9cb0070e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-baron/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7e41ee93e4606596561a",
+    "url": "/react-baron/static/js/runtime~main.7e41ee93.js"
+  },
+  {
+    "revision": "9e4dfe982771b997ed35",
+    "url": "/react-baron/static/js/main.9e4dfe98.chunk.js"
+  },
+  {
+    "revision": "c8582078af6d7f96cce5",
+    "url": "/react-baron/static/js/2.c8582078.chunk.js"
+  },
+  {
+    "revision": "9e4dfe982771b997ed35",
+    "url": "/react-baron/static/css/main.d5eb32a1.chunk.css"
+  },
+  {
+    "revision": "4e0aca032c5ba7f4610cb7f6ce305b15",
+    "url": "/react-baron/index.html"
+  }
+];
