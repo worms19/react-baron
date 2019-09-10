f0755b4b84f0f96fe502ccfb97a82d1a80aeb497
damien <anselmidamien@gmail.com>
2019-09-10 21:18:36 +0200

    test

--- app.js
+++ app.js
@@ -37,8 +37,11 @@ app.use('/graphql', graphQlHttp({
 
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
+app.get("*", (req, res) => {
+  let url = path.join(__dirname, '../frontend/build', 'index.html');
+  if (!url.startsWith('/app/')) // since we're on local windows
+    url = url.substring(1);
+  res.sendFile(url);
 });
 
 mongoose.connect(
