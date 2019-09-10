6d419525538d975014dd5d0630aa3da7fa8353cc
da <a>
2019-09-10 10:23:25 +0200

    deploy2

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use('/graphql', graphQlHttp({
 }));
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/frontend/build/index.html`);
+  res.sendFile(`${__dirname}../frontend/build/index.html`);
 });
 
 mongoose.connect(
