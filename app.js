2d58570cb8f23f3a6a3e5eb97ed15d8c82e8a851
da <a>
2019-09-10 10:51:58 +0200

    frontend

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.use('/graphql', graphQlHttp({
   graphiql: true,
 }));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
 });
 
 mongoose.connect(
