de86e27f0b25d4eae454211785dc5fd2ef904c3e
dam <anselmidamien@gmail.com>
2019-09-14 14:05:55 -0700

    test

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use('/graphql', graphQlHttp({
 app.use(express.static('frontend/build'));
 //app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
 });
 
