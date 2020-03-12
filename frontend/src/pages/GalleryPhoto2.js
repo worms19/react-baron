be41122d1f3cbf3f0e707255a2f53694869368ad
worms19 <anselmidamien@gmail.com>
2020-03-12 19:36:35 +0100

    use thumbnails

--- frontend/src/pages/GalleryPhoto2.js
+++ frontend/src/pages/GalleryPhoto2.js
@@ -12,126 +12,90 @@ export default class GalleryPhoto2 extends React.Component {
     super();
       this.state = {
           media: [
-              { src:'img/br-img/190705_Baron CrÉne_003.jpg',
-                index: 0
-              },
-              { src:'img/br-img/190705_Baron CrÉne_006.jpg',
-                index: 1
-              },
-              { src:'img/br-img/190705_Baron CrÉne_009.jpg',
-                index: 2
-              },
-              { src:'img/br-img/190705_Baron CrÉne_010.jpg',
-                index: 3
-              },
-              { src:'img/br-img/190705_Baron CrÉne_015.jpg',
-                index: 4
-              },
-              { src:'img/br-img/190705_Baron CrÉne_016.jpg',
-                index: 5
-              },
-              { src:'img/br-img/190705_Baron CrÉne_020.jpg',
-                index: 6
-              },
-              { src:'img/br-img/190705_Baron CrÉne_022.jpg',
-                index: 7
-              },
-              { src:'img/br-img/190705_Baron CrÉne_026.jpg',
-                index: 8
-              },
-              { src:'img/br-img/190705_Baron CrÉne_036.jpg',
-                index: 9
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_006_WEB.jpg',
-                index: 10
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_008_WEB.jpg',
-                index: 11
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_009_WEB.jpg',
-                index: 12
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_010_WEB.jpg',
-                index: 13
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_021_WEB.jpg',
-                index: 14
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_033_WEB.jpg',
-                index: 15
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_037_WEB.jpg',
-                index: 16
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_056_WEB.jpg',
-                index: 17
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_058_WEB.jpg',
-                index: 18
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_066_WEB.jpg',
-                index: 19
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_069_WEB.jpg',
-                index: 20
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_075_WEB.jpg',
-                index: 21
-              },
-              { src:'img/br-img/190927_Baron CrÉne_Portraits_082_WEB.jpg',
-                index: 22
-              },
-              { src:'img/br-img/Baron Crane - International-4277.jpg',
-                index: 23
-              },
-              { src:'img/br-img/Baron Crane - International-4412.jpg',
-                index: 24
-              },
-              { src:'img/br-img/Baron Crane - International-4416.jpg',
-                index: 25
-              },
-              { src:'img/br-img/Baron Crane - International-4455.jpg',
-                index: 26
-              },
-              { src:'img/br-img/Baron Crane - Portrait-.jpg',
-                index: 27
-              },
-              { src:'img/br-img/DSC_0639.jpg',
-                index: 28
-              },
-              { src:'img/br-img/DSC_9395.jpg',
-                index: 29
-              },
-              { src:'img/br-img/DSC_9417.jpg',
-                index: 30
-              },
-              { src:'img/br-img/DSC_9479.jpg',
-                index: 31
-              },
-              { src:'img/br-img/DSC_9494.jpg',
-                index: 32
-              },
-              { src:'img/br-img/DSC_9530.jpg',
-                index: 33
-              },
-              { src:'img/br-img/DSC_9610.jpg',
-                index: 34
-              },
-              { src:'img/br-img/DSC_9702.jpg',
-                index: 35
-              },
-              { src:'img/br-img/DSC_9807.jpg',
-                index: 36
-              },
-              { src:'img/br-img/DSC_9833.jpg',
-                index: 37
-              },
+              { src:'img/br-img/thumbnails/190705_Baron CrÉne_003_tn.jpg',   index: 0},
+              { src:'img/br-img/thumbnails/190705_Baron CrÉne_006_tn.jpg', index: 1},
+              { src:'img/br-img/thumbnails/190705_Baron CrÉne_009_tn.jpg', index: 2},
+              { src:'img/br-img/thumbnails/190705_Baron CrÉne_010_tn.jpg', index: 3},
+              { src:'img/br-img/thumbnails/190705_Baron CrÉne_015_tn.jpg', index: 4},
+              { src:'img/br-img/thumbnails/190705_Baron CrÉne_016_tn.jpg', index: 5},
+              { src:'img/br-img/thumbnails/190705_Baron CrÉne_020_tn.jpg', index: 6},
+              { src:'img/br-img/thumbnails/190705_Baron CrÉne_022_tn.jpg', index: 7},
+              { src:'img/br-img/thumbnails/190705_Baron CrÉne_026_tn.jpg', index: 8},
+              { src:'img/br-img/thumbnails/190705_Baron CrÉne_036_tn.jpg', index: 9},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_006_WEB_tn.jpg', index: 10},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_008_WEB_tn.jpg', index: 11},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_009_WEB_tn.jpg', index: 12},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_010_WEB_tn.jpg', index: 13},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_021_WEB_tn.jpg', index: 14},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_033_WEB_tn.jpg', index: 15},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_037_WEB_tn.jpg', index: 16},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_056_WEB_tn.jpg', index: 17},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_058_WEB_tn.jpg', index: 18},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_066_WEB_tn.jpg', index: 19},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_069_WEB_tn.jpg', index: 20},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_075_WEB_tn.jpg', index: 21},
+              { src:'img/br-img/thumbnails/190927_Baron CrÉne_Portraits_082_WEB_tn.jpg', index: 22},
+              { src:'img/br-img/thumbnails/Baron Crane - International-4277_tn.jpg', index: 23},
+              { src:'img/br-img/thumbnails/Baron Crane - International-4412_tn.jpg', index: 24},
+              { src:'img/br-img/thumbnails/Baron Crane - International-4416_tn.jpg', index: 2},
+              { src:'img/br-img/thumbnails/Baron Crane - International-4455_tn.jpg', index: 26},
+              { src:'img/br-img/thumbnails/Baron Crane - Portrait-_tn.jpg', index: 27},
+              { src:'img/br-img/thumbnails/DSC_0639_tn.jpg', index: 28},
+              { src:'img/br-img/thumbnails/DSC_9395_tn.jpg', index: 29},
+              { src:'img/br-img/thumbnails/DSC_9417_tn.jpg', index: 30},
+              { src:'img/br-img/thumbnails/DSC_9479_tn.jpg', index: 31},
+              { src:'img/br-img/thumbnails/DSC_9494_tn.jpg', index: 32},
+              { src:'img/br-img/thumbnails/DSC_9530_tn.jpg', index: 33},
+              { src:'img/br-img/thumbnails/DSC_9610_tn.jpg', index: 34},
+              { src:'img/br-img/thumbnails/DSC_9702_tn.jpg', index: 35},
+              { src:'img/br-img/thumbnails/DSC_9807_tn.jpg', index: 36},
+              { src:'img/br-img/thumbnails/DSC_9833_tn.jpg', index: 37},
 
           ],
           photoIndex: 0,
           isOpen: false,
       };
-
+      this.images = [
+          'img/br-img/190705_Baron CrÉne_003.jpg',
+          'img/br-img/190705_Baron CrÉne_006.jpg',
+          'img/br-img/190705_Baron CrÉne_009.jpg',
+          'img/br-img/190705_Baron CrÉne_010.jpg',
+          'img/br-img/190705_Baron CrÉne_015.jpg',
+          'img/br-img/190705_Baron CrÉne_016.jpg',
+          'img/br-img/190705_Baron CrÉne_020.jpg',
+          'img/br-img/190705_Baron CrÉne_022.jpg',
+          'img/br-img/190705_Baron CrÉne_026.jpg',
+          'img/br-img/190705_Baron CrÉne_036.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_006_WEB.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_008_WEB.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_009_WEB.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_010_WEB.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_021_WEB.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_033_WEB.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_037_WEB.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_056_WEB.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_058_WEB.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_066_WEB.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_069_WEB.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_075_WEB.jpg',
+          'img/br-img/190927_Baron CrÉne_Portraits_082_WEB.jpg',
+          'img/br-img/Baron Crane - International-4277.jpg',
+          'img/br-img/Baron Crane - International-4412.jpg',
+          'img/br-img/Baron Crane - International-4416.jpg',
+          'img/br-img/Baron Crane - International-4455.jpg',
+          'img/br-img/DSC_0639.jpg',
+          'img/br-img/DSC_9395.jpg',
+          'img/br-img/DSC_9417.jpg',
+          'img/br-img/DSC_9479.jpg',
+          'img/br-img/DSC_9494.jpg',
+          'img/br-img/DSC_9530.jpg',
+          'img/br-img/DSC_9610.jpg',
+          'img/br-img/DSC_9702.jpg',
+          'img/br-img/DSC_9807.jpg',
+          'img/br-img/DSC_9833.jpg',
+          'img/br-img/thumbnails',
+          'img/br-img/thumbnails.zip'
+      ];
   }
 
 
@@ -146,7 +110,7 @@ export default class GalleryPhoto2 extends React.Component {
 
       const { photoIndex, isOpen } = this.state;
       const { media } = this.state;
-      const images = media.map(m  =>  m.src);
+      const { images } = this;
       const isMobile = helpers.isMobileDevice();
       const row = isMobile === true ? 1 : 3;
 
